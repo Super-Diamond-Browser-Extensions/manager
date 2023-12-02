b7be7edbac6b0c02ad67c0118f424f06ae19c96e
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-02 14:24:22 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'superdiamondbrowserextensions-offline';
+const CACHE_DATA = [
+  '/service-worker.js',
+  '/load-service-worker.html',
+  '/discDownload.html',
+  '/discImport.html',
+  '/index.html',
+  '/loadExtensions.html',
+  '/superDiamondDriveImport.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_DATA);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
